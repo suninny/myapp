4978ae09160dc4c1b2aa546545a855c73831e7fc
suninny <suninny@gmail.com>
2011-09-13 07:41:45 +0800

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -34,4 +34,5 @@ end
 group :test do
   gem 'rspec'
   gem 'webrat'
+  gem 'spork'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,33 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  unless defined?(Rails)
+    require File.dirname(__FILE__) + "/../config/environment"
+  end
+  
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  
+end
+
+# --- Instructions ---
+# - Sort through your spec_helper file. Place as much environment loading 
+#   code that you don't normally modify during development in the 
+#   Spork.prefork block.
+# - Place the rest under Spork.each_run block
+# - Any code that is left outside of the blocks will be ran during preforking
+#   and during each_run!
+# - These instructions should self-destruct in 10 seconds.  If they don't,
+#   feel free to delete them.
+#
+
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -24,4 +51,5 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  ActiveSupport::Dependencies.clear 
 end
