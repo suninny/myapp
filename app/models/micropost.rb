36077c055320f249658c9b266d7cc42937c87336
suninny <suninny@gmail.com>
2011-10-02 20:52:27 +0800

    trans commit

---
+++ app/models/micropost.rb
@@ -0,0 +1,17 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :content
+
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+describe Micropost do
+  before(:each) do
+    @user = Factory(:user)
+    @attr = { :content => "value for content" }
+  end
+  it "should create a new instance given valid attributes" do
+    @user.microposts.create!(@attr)
+  end
+  describe "user associations" do
+    before(:each) do
+      @micropost = @user.microposts.create(@attr)
+    end
+    it "should have a user attribute" do
+      @micropost.should respond_to(:user)
+    end
+    it "should have the right associated user" do
+      @micropost.user_id.should == @user.id
+      @micropost.user.should == @user
+    end
+  end
+
+  describe "micropost associations" do
+    before(:each) do
+      @user = User.create(@attr)
+    end
+    it "should have a microposts attribute" do
+      @user.should respond_to(:microposts)
+    end
+  end
+end
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
